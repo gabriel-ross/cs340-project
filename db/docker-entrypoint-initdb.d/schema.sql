f2635c78ecfa29c8248563fcc9da0d8db2f05f90
Gabriel Ross <Gabriel.Ross@outlook.com>
2022-02-19 10:45:34 -0800

    some more sql stuff

--- db/docker-entrypoint-initdb.d/schema.sql
+++ db/docker-entrypoint-initdb.d/schema.sql
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
+
 -- Reset db
 
-=======
->>>>>>> main
 DROP TABLE IF EXISTS `Pokemon`;
 DROP TABLE IF EXISTS `Types`;
 DROP TABLE IF EXISTS `Moves`;
@@ -34,22 +32,48 @@ CREATE TABLE `Moves` (
     PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `Pokemon` (
-    `id` int(11) NOT NULL,
+CREATE TABLE `Generations` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
     `name` varchar(255) NOT NULL,
-    `primary_type` int(11) NOT NULL,
-    `secondary_type` int(11),
-    `generation` int(11) NOT NULL,
     PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `Pokemon` (
-    `id` int(11) NOT NULL,
-    `name` varchar(255) NOT NULL,
-    `primary_type` int(11) NOT NULL,
-    `secondary_type` int(11),
-    `generation` int(11) NOT NULL,
-    PRIMARY KEY (`id`)
+CREATE TABLE `Pokemon_Moves` (
+    `pokemon_id` int(11) NOT NULL,
+    `move_id` int(11) NOT NULL,
+    PRIMARY KEY (`pokemon_id`, `move_id`),
+    FOREIGN KEY (`pokemon_id`) REFERENCES `Pokemon` (`id`),
+    FOREIGN KEY (`move_id`) REFERENCES `Moves` (`id`)
 );
 
 -- Insert preliminary data
+
+INSERT INTO `Types` (`name`) VALUES
+('Normal'),
+('Fire'),
+('Water'),
+('Grass'),
+('Electric'),
+('Ice'),
+('Fighting'),
+('Poison'),
+('Ground'),
+('Flying'),
+('Psychic'),
+('Bug'),
+('Rock'),
+('Ghost'),
+('Dark'),
+('Dragon'),
+('Steel'),
+('Fairy');
+
+INSERT INTO `Generations` (`name`) VALUES
+('I'),
+('II'),
+('III'),
+('IV'),
+('V'),
+('VI'),
+('VII'),
+('VIII');
